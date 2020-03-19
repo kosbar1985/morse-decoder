d1c5070cbd4d5c0d5e41243087b70069e65b34b7
unknown <C:\Users\\AppData\Roaming\The Bat!>
2020-03-19 14:26:43 +0300

    add decicion

--- src/index.js
+++ src/index.js
@@ -38,7 +38,63 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let clone = Object.assign({}, MORSE_TABLE);
+    let arr = expr.split("");
+    let arr_new = [];
+    let i = 0;
+    let arr_new2 = [];
+    let result = '';
+
+    for (let j = 0; j < arr.length / 10; j++) {
+        arr_new[j] = arr.slice(i, i + 10);
+        i = i + 10;
+    }
+    for (i = 0; i < arr_new.length; i++) {
+        arr_new2[i] = arr_new[i].join("");
+
+    }
+    for (let key in clone) {
+
+        let new_key = transform(key);
+        clone[new_key] = clone[key];
+        delete clone[key];
+
+    }
+    for (i = 0; i < arr_new2.length; i++) {
+        if (arr_new2[i] == "**********") {
+                result += " ";
+        } else {
+        
+        for (let key in clone) {
+            if (key == arr_new2[i]) {
+                result += clone[key];
+            } 
+            }
+        }
+    }
+return(result);
+    
+
+    function transform(x) {
+
+        let arr_res = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'];
+        let arr_morse = x.split("");
+        for (let j = 0; j < arr_morse.length; j++) {
+            if (arr_morse[j] == '.') {
+                arr_res.push('1');
+                arr_res.push('0');
+                arr_res.splice(0, 2);
+            } else if (arr_morse[j] == '-') {
+                arr_res.push('1');
+                arr_res.push('1');
+                arr_res.splice(0, 2);
+            } else {
+                return;
+            }
+        }
+
+        return arr_res.join().split(",").join("");
+    }
 }
 
 module.exports = {
